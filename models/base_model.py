689b4fe4275afe9d2f4f51e9d8a043279b86fd24
symonmuchemi <simonkenya846@gmail.com>
2024-02-08 18:59:17 +0300

    base task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,20 +5,22 @@ from datetime import datetime
 
 
 class BaseModel:
-    """blueprint for all other classes in the module
-
-    Returns:
-        BaseModel: an instance
-    """
-    id = str(uuid.uuid4()) # The unique id
-    created_at = datetime.now()
-    update_at = created_at
-    
-    def __str__(self):
-        """prints a short description of the instance
-        """
-        print(f"[s{self.__class__}] ({self.id}) {self.__dict__}")   
-
+    def __init__(self, *args, **kwargs):
+        """Intializes the """
+        self.id = str(uuid.uuid4()) # The unique id
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
+        
+        if len(args) == 0:
+            if len(kwargs) > 0:
+                for key, val in kwargs.items():
+                    if key == "id":
+                        self.id = val
+                    if key == "created_at":
+                        self.created_at = str(val)
+                    if key == "updated_at":
+                        self.update_at = str(val)
+        
     def save(self):
         """Update the 'update_at' with the current datetime"""
         self.update_at = datetime.now()
@@ -30,8 +32,13 @@ class BaseModel:
             dict: a dictionary of instance attributes including
             the __class__ attribute
         """
-        self.__dict__["__class__"] = self.__class__
         self.created_at = (self.created_at).strftime("%Y-%m-%dT%H:%M:%S.%f")
         self.update_at = (self.update_at).strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.__dict__["__class__"] = self.__class__
         
         return self.__dict__
+
+    def __str__(self):
+        """prints a short description of the instance
+        """
+        return f"[s{self.__class__}] ({self.id}) {self.__dict__}"
