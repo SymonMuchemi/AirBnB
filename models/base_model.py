144fa5893307731a16c2f91d60fc724961341b8e
symonmuchemi <simonkenya846@gmail.com>
2024-02-13 01:47:15 +0300

    creates instance from kwargs updated __init__() method to handle key worded arguments by directly adding attributes to the BaseModel's __dict__

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,24 @@ DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
     """the blueprint for model classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        
+    def __init__(self, *args, **kwargs):
+        """The class instance creator"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            # set attributes from key-worded arguments
+            for key, val in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(
+                        val, DATE_FORMAT
+                    )
+                elif key == "id":
+                    self.__dict__[key] = str(val)
+                else:
+                    self.__dict__[key] = val
+
     def __str__(self):
         """Return a formal representation of the BaseModel obj"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
