67cc6ef1dcfbf2c54acc585bc2e1127f391316ff
symonmuchemi <simonkenya846@gmail.com>
2024-03-05 11:13:00 +0300

    new beginnings

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""instantiate the file storage class and calls the reload method"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
